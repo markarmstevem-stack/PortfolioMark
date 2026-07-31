21b84f696261c612bfd4399335ba29c993c97e2d
markarmstevem-stack <markarmstevem@gmail.com>
2026-07-31 15:35:55 +0800

    Improve mobile responsiveness

--- Portfolio.php
+++ Portfolio.php
@@ -67,7 +67,7 @@ $OtherList = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $siteTitle; ?></title>
-    <link rel="stylesheet" href="Portfolio2.css">
+    <link rel="stylesheet" href="Portfolio2.css?v=2">
 </head>
 
 <body>
